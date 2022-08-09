0e88fe28f669b4fa3ca2e2effb03434b195b3b70
Hecea <81994410+Hecea@users.noreply.github.com>
2022-08-09 10:06:01 +0200

    Add files via upload
    description de code pour un premier programme

---
+++ hello.c
@@ -0,0 +1,25 @@
+/* fichier d entete .h  stdio fait appel au  standard input output  du langage C
+header file in order to use the printf function */
+
+#include <stdio.h>
+
+
+int main () {
+  printf("Hello, world!\n");
+  return 0;
+}
+
+/*
+int for integer variable
+main for main function, first to run
+\n for a new line
+return 0 terminates the main()
+{Curly brackets indicate the beginning and end of a function also called a code block}
+Pour compiler utiliser l outil de compilation gcc avec la commande :
+gcc momfichier.c -o  lenomdefichierquejeveuxensortie
+l argument -o  cree un fichier executable binaire  monfichier.exe
+puis pour run the programme tapez:  ./ momfichie.exe 
+ */
+
+	
+ 
